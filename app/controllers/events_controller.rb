95081b91395281fc3f036da9d18cf1ec355367ad
Anchorite1001 <yangwu.1415@gmail.com>
2021-02-16 01:52:01 +1100

    event controller change

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,13 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = Event.create event_params
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :introduction, :date, :user, :restaurant_id)
+  end
 end
