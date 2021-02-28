543177a34f3a91e1f853fabe9048985d4878e1f4
Mai Nguyen <chimai.dk@gmail.com>
2021-02-28 21:47:05 +1100

    Changed session_store to heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # this needs to have the domain added here for the cookie to work once it goes to heroku
 
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_i-feel-like', domain: 'i-feel-like-json-api'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_i-feel-like', domain: 'heroku'
 else
   Rails.application.config.session_store :cookie_store, key: '_i-feel-like'
 end
