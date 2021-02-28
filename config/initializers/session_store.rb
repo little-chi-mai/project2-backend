3335d536086694c88c69c04f904d55ff00b490f5
Mai Nguyen <chimai.dk@gmail.com>
2021-02-28 22:40:10 +1100

    Changed session store again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # this needs to have the domain added here for the cookie to work once it goes to heroku
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_i-feel-like', domain: 'heroku', secure: true, httponly: false, same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_i-feel-like', domain: 'https://agile-tor-91190.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_i-feel-like'
 end
